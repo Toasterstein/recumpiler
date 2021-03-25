f84dea1f5c6954d12390c672dc78562b49a9d690
Toasterstein <Toasterstein@protonmail.com>
2021-03-25 18:58:24 -0400

    adding fallback to shortname

--- recumpiler/mutators_emoji_data.py
+++ recumpiler/mutators_emoji_data.py
@@ -6,9 +6,11 @@ import emoji_data_python
 def get_emoji_from_data(token: str):
     if len(token) < 4:
         return None
-    else:
-        emojis = [emoji.char for emoji in emoji_data_python.find_by_name(token)]
-        if emojis:
-            return random.choice(emojis)
-        else:
-            return None
+    emojis = [emoji.char for emoji in emoji_data_python.find_by_name(token)]
+    if emojis:
+        return random.choice(emojis)
+    # TODO: maybe make find by shortname a separate modifier
+    emojis = [emoji.char for emoji in emoji_data_python.find_by_shortname(token)]
+    if emojis:
+        return random.choice(emojis)
+    return None
