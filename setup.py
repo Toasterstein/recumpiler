f3335508aec6950af80d9fdc153826e7bf8153a1
Toasterstein <Toasterstein@protonmail.com>
2021-03-26 19:34:10 -0400

    adding requirement `spacy`

--- setup.py
+++ setup.py
@@ -128,6 +128,7 @@ setup(
         "emoji>=0.6.0,<1.0.0",
         "emotlib>=1.0.1,<2.0.0",
         "emoji-data-python>=1.5.0,<2.0.0",
+        "spacy>=3.0.5,<4.0.0",
     ],
     setup_requires=[
         "nltk>=3.4.5,<4.0.0",
