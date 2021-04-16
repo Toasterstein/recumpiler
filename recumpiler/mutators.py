20d44d65e1f894c05bfed5bcb9235d7f293aeb92
Toasterstein <Toasterstein@protonmail.com>
2021-04-16 17:56:11 -0400

    adding ability for words to be upside down yet still preserve left to right readability

--- recumpiler/mutators.py
+++ recumpiler/mutators.py
@@ -944,6 +944,8 @@ def recumpile_token(token: str) -> str:
 
         if decision(upside_down_word_probability):
             token = word_upside_downer(token)
+        elif decision(upside_down_word_probability):
+            token = word_upside_downer_preserve_char_order
 
         fucked_token = knotter(fuckyer(reeeer(rawrer(garbage(owoer(cummer(token)))))))
 
@@ -1293,6 +1295,14 @@ def word_upside_downer(token: str) -> str:
     token = upsidedown.transform(token)
 
     return token
+
+
+@logged_mutator
+def word_upside_downer_preserve_char_order(token: str) -> str:
+    new_token = []
+    for char in token:
+        new_token.append(upsidedown.transform(char))
+    return "".join(new_token)
 
 
 @logged_mutator
